db30b1e0aa66b5e5dc8685a09f1125c5f00483b6
Petr Fila <petrfila@192-168-1-3.tpgi.com.au>
2019-02-03 18:03:46 +1100

    first screen with UILabel

--- Protocol_challenge/ViewController.swift
+++ Protocol_challenge/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    lazy var label: UILabel = {
+        let field = UILabel()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.font = UIFont.systemFont(ofSize: 14)
+        field.backgroundColor = UIColor.yellow
+        return field
+    }()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+        
+        layoutProperties()
     }
 
+    private func layoutProperties() {
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+    }
 
 }
 
